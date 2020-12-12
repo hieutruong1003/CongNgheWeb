62464393b9732ee31f83abc56eb6e3f9b41f7a1f
hieutruong1003 <hieutruong12071999@gmail.com>
2020-12-12 10:48:52 +0700

    update

--- Code_backend/backend/app.py
+++ Code_backend/backend/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 
 dp_ip = os.getenv("db_ip")
 ConnectionData = {}
-ConectionData['user'] = 'postgres'
-ConectionData['password'] = 'password'
+ConnectionData['user'] = 'postgres'
+ConnectionData['password'] = 'password'
 ConnectionData['host'] = str(db_ip)
 ConnectionData['port'] = '5432'
 ConnectionData['database'] = 'teamhhl'
